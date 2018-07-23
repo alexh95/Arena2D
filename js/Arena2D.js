0092c0f963b4ff8ce7650e41d6a9d3e731f8c168
alexh95 <alexhuruiala@gmail.com>
2018-07-24 00:51:53 +0300

    added image loading and drawing

--- js/Arena2D.js
+++ js/Arena2D.js
@@ -1,19 +1,69 @@
-const renderer = {};
+class Renderer {
+
+	constructor() {
+		this.canvas = document.getElementById('canvas');
+		this.canvas.width = window.innerWidth;
+		this.canvas.height = window.innerHeight;
+		this.context = canvas.getContext('2d');
+	}
+
+}
+
+class ImageStore {
+
+	constructor() {
+		this.images = [];
+		this.imageLoadedCount = 0;
+		this.imagesToLoad = 0;
+	}
+
+	loadImage(src) {
+		++this.imagesToLoad;
+		const image = new Image();
+		const reference = this;
+		image.onload = () => ++reference.imageLoadedCount;
+		image.src = src;
+		const index = this.images.length;
+		this.images.push(image);
+		return index;
+	}
+
+	isLoadingFinished() {
+		return this.imageLoadedCount == this.imagesToLoad;
+	}
+
+}
+
+let renderer = null;
+let imageStore = null;
+
+let ballIndex;
+
 init();
 
 function init() {
-	console.log('Arena2D 0.0.1');
+	console.log('Arena2D 0.0.2');
 
-	renderer.canvas = document.getElementById('canvas');
-	renderer.canvas.width = window.innerWidth;
-	renderer.canvas.height = window.innerHeight;
-	renderer.context = canvas.getContext('2d');
+	renderer = new Renderer();
+	imageStore = new ImageStore();
+	ballIndex = imageStore.loadImage('res/ball.png');
 
-	loop();
+	startLoop();
+}
+
+function startLoop() {
+	if (imageStore.isLoadingFinished()) {
+		window.requestAnimationFrame(loop);
+	} else {
+		window.requestAnimationFrame(startLoop);
+	}
 }
 
 function loop() {
+	// TODO(alex): test with request frame as the last instruction
 	window.requestAnimationFrame(loop);
 
 	renderer.context.clearRect(0, 0, renderer.canvas.width, renderer.canvas.height);
+
+	renderer.context.drawImage(imageStore.images[ballIndex], 200, 200);
 }
