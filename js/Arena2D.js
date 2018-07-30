1327c97b9988be51842c0aa4a1d4d7f198b87cb0
Alexandru Huruiala <Alexandru.Huruiala@catalysts.cc>
2018-07-30 23:14:47 +0300

    basic entity system

--- js/Arena2D.js
+++ js/Arena2D.js
@@ -34,19 +34,55 @@ class ImageStore {
 
 }
 
+class V3 {
+
+	constructor(x, y, z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+}
+
+const EntityTypes = Object.freeze({
+	PLAYER: 0,
+	WALL: 1,
+	BALL: 2
+});
+
+class Entity {
+
+	constructor(type, p, center) {
+		this.type = type;
+		this.p = p;
+		this.center = center;
+	}
+
+}
+
+const entityTypeToImage = {};
+const entities = [];
+
 let renderer = null;
 let imageStore = null;
-
-let ballIndex;
 
 init();
 
 function init() {
-	console.log('Arena2D 0.0.2');
+	console.log('Arena2D 0.0.3');
 
 	renderer = new Renderer();
 	imageStore = new ImageStore();
-	ballIndex = imageStore.loadImage('res/ball.png');
+	entityTypeToImage[EntityTypes.WALL] = imageStore.loadImage('res/wall.png');
+	entityTypeToImage[EntityTypes.BALL] = imageStore.loadImage('res/ball.png');
+
+	const wall1 = new Entity(EntityTypes.WALL, new V3(128, 128, 0), new V3(128, 128, 0));
+	const wall2 = new Entity(EntityTypes.WALL, new V3(renderer.canvas.width - 128, 128, 0), new V3(128, 128, 0));
+	const ball = new Entity(EntityTypes.BALL, new V3(250, 250, 0), new V3(128, 128, 0));
+	
+	entities.push(wall1);
+	entities.push(wall2);
+	entities.push(ball);
 
 	startLoop();
 }
@@ -65,5 +101,10 @@ function loop() {
 
 	renderer.context.clearRect(0, 0, renderer.canvas.width, renderer.canvas.height);
 
-	renderer.context.drawImage(imageStore.images[ballIndex], 200, 200);
+	for (const entity of entities) {
+		renderer.context.save();
+		renderer.context.translate(-entity.center.x, -entity.center.y);
+		renderer.context.drawImage(imageStore.images[entityTypeToImage[entity.type]], entity.p.x, entity.p.y);
+		renderer.context.restore();
+	}
 }
